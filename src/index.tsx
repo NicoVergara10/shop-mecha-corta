6a6a1c7d9355a45b3d46f75ca04e3a5224495792
candelaojeda <ojedacandelaco@gmail.com>
2023-04-08 16:03:31 -0300

    added theme provider and create theme with font families and colors palette.

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createTheme, ThemeProvider } from "@mui/material";
+import { colors } from "./utils/colors";
+
+const mechaCortaTheme = createTheme({
+  typography: {
+    fontFamily: [
+      "Inter",
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      "Oxygen",
+      "Ubuntu",
+      "Cantarell",
+      '"Fira Sans"',
+      '"Droid Sans"',
+      '"Helvetica Neue"',
+      "sans-serif",
+    ].join(","),
+  },
+  palette: {
+    primary: {
+      main: colors.white,
+    },
+    secondary: {
+      main: colors.white,
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={mechaCortaTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
