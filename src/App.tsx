57840b99f7c6be9c84740d98b8f8873c309ce185
candelaojeda <ojedacandelaco@gmail.com>
2023-04-08 16:01:49 -0300

    added useStyles and cssBaseline at material

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { CssBaseline, Box, Theme } from "@mui/material";
+import { makeStyles } from "tss-react/mui";
+import { colors } from "./utils/colors";
+import Navbar from "./components/Navbar";
+
+const useStyles = makeStyles()((theme: Theme) => ({
+  app: {
+    display: "flex",
+    height: "100vh",
+    backgroundColor: colors.white,
+  },
+  pageContainer: {
+    flexGrow: 1,
+    marginTop: "64px",
+    "@media (max-width: 600px)": {
+      marginTop: "56px",
+    },
+    fontWeight: 700,
+    fontSize: 24,
+    lineHeight: "120%",
+    width: "100%",
+  },
+}));
 
 function App() {
+  const { classes } = useStyles();
+
   return (
-    <div className="App">
-      hi
+    <div className={classes.app}>
+      <CssBaseline />
+      <React.Fragment>
+        <Navbar />
+      </React.Fragment>
     </div>
   );
 }
